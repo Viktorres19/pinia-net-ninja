383b56a4a907ac457fc2fc48d4cc7591ea0535d2
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-05-13 08:17:18 +0300

    04 Accessing Store State

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,27 @@
 <template>
-  <main>
-    <header>
-      <img src="@/assets/pinia-logo.svg" alt="pinia logo">
-        <h1>Pinia Tasks</h1>
-    </header>
-  </main>
+	<main>
+		<header>
+			<img src="@/assets/pinia-logo.svg" alt="pinia logo">
+			<h1>Pinia Tasks</h1>
+		</header>
+		<div class="task-list">
+			<div v-for="task in taskStore.tasks">
+				<TaskDetails :task="task"/>
+			</div>
+		</div>
+	</main>
 </template>
 
 <script>
-export default {
+import TaskDetails from '@/components/TaskDetails.vue'
+import {useTaskStore} from '@/stores/taskStore'
 
+export default {
+  components: {TaskDetails},
+  setup() {
+    const taskStore = useTaskStore()
+
+    return {taskStore}
+  }
 }
 </script>
